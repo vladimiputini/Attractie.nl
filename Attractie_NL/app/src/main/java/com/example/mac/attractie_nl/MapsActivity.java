c0d5a82fa398f6c6e88cee907fa1cf8078a2def8
Unknown <thomas.shamoian@student.rocleiden.nl>
2019-01-28 15:21:24 +0100

    last
    Edit

--- Attractie_NL/app/src/main/java/com/example/mac/attractie_nl/MapsActivity.java
+++ Attractie_NL/app/src/main/java/com/example/mac/attractie_nl/MapsActivity.java
@@ -26,7 +26,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        
+
 
     }
 
